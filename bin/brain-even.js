b94ea2e8614cc08b6be32d8005a3a0e8a8fd6d16
bitsen03 <bitsen03@mail.ru>
2022-11-16 12:15:56 +0300

    fix

---
+++ bin/brain-even.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+import { name }from '../src/cli.js'
+
+console.log('Welcome to the Brain Games!')
+ function gameRandom() {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    for (let i = 0; i < 3; i++) {
+        const randomNumber = Math.floor(Math.random() * 100)
+        if (randomNumber % 2 > 0) {
+            console.log(`Question: ${randomNumber}`);
+            const answer = readlineSync.question('Your answer: ');
+            if (answer === 'no') {
+                console.log('Correct!');
+            }
+            else {
+                console.log(`${answer} is wrong answer ;(. Correct answer was 'no'. \nLet's try again, Bill!`);
+                return;
+            }
+
+        }
+        else if (randomNumber % 2 === 0) {
+            console.log(`Question: ${randomNumber}`);
+            const answer = readlineSync.question('Your answer: ');
+            if (answer === 'yes') {
+                console.log('Correct!');
+            }
+            else {
+                console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, Bill!`);
+                return;
+            }
+        }
+        
+    }
+    console.log(`Congratulations, ${name}!`)
+}
+gameRandom()
