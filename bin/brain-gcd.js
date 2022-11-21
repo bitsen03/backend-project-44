3f0aa62976cbaaef812ab07049807775eeff92d7
bitsen03 <bitsen03@mail.ru>
2022-11-21 11:27:34 +0300

    new brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,12 +1,8 @@
 import readlineSync from 'readline-sync'
-const greatestCommonDivisor = () => {
-console.log('Find the greatest common divisor of given numbers.')
-let result 
-const arr1 = []
-const arr2 = []
-let rightAnswer
-
-for (let i = 0; i < 3; i += 1){
+ const gcd = () => {
+for (let i = 0; i < 3; i++){
+    const arr1 = []
+    const arr2 = []
     let randomNumber1 = Math.floor(Math.random() * 100)
     let randomNumber2 = Math.floor(Math.random() * 100)
 
@@ -14,63 +10,39 @@ for (let i = 0; i < 3; i += 1){
 
     const answer = readlineSync.question('Your answer: ');
 
-for (let z = 0; z <= randomNumber1; z += 1) {
-   
-if (randomNumber1 % z === 0){
+    for (let z = 0; z <= randomNumber1 ; z++){
+        if (randomNumber1 % z === 0){
 
 arr1.push(z)
-if (z === randomNumber1) {
-    arr1.push('r')
-}
 
-}
-}
-for (let j = 0; j <= randomNumber2; j += 1) {
-    
-if (randomNumber2 % j === 0){
-
-arr2.push(j)
-if (j === randomNumber2) {
-    arr2.push('r')
-}
-
-}
-}
-console.log('arr1 ' + arr1)
-console.log('arr2 ' + arr2)
-const filterArr = arr1.filter((x) => arr2.includes(x));
-console.log(filterArr)
-for (let k = 0; k < filterArr.length; k++ ) {
-    if (filterArr[k] === 'r') {
-       
-        rightAnswer = filterArr[k - 1]
-     
+        }
     }
-}
+    for (let x = 0; x <= randomNumber2 ; x++){
+        if (randomNumber2 % x === 0){
+    
+    arr2.push(x)
+    
+        }
+    }
 
-// console.log('arr1 ' + arr1)
-// console.log('arr2 ' + arr2)
-// console.log(filterArr)
-// console.log(rightAnswer)
-if (answer === String(rightAnswer)) {
-    console.log('correct!')
-}
-else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, Sam!`)
-    return
-}
-
-}
-// const filterArr = arr1.filter((x) => arr2.includes(x));
-// console.log(filterArr)
-// for (let i = 0; i < filterArr.length; i++ ) {
-//     if (filterArr[i] === 'r') {
-//        rightAnswer = filterArr[i - 1]
-     
-//     }
+    const filterArr = arr1.filter((x) => arr2.includes(x));
+    
+    
+    const rightAnswer = filterArr[filterArr.length - 1]
+    if (answer === String(rightAnswer)) {
+        console.log('correct!')
+    }
+    else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, Sam!`)
+        return
+    }
+    
+    }
   
-// }
-
-
+    
 }
-greatestCommonDivisor()
+export { gcd }
+
+
+
+
