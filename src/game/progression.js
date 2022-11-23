d033977b1b7cdd092e51580a7daf17b168d459e0
vitaliy <bitsen03@mail.ru>
2022-11-23 23:29:08 +0300

    fix progression

--- src/game/progression.js
+++ src/game/progression.js
@@ -6,15 +6,16 @@ export default function brainProgression() {
   console.log(`Hello, ${name}!`);
   console.log('What number is missing in the progression?');
   for (let i = 0; i < 3; i += 1) {
-    const randomNumber2 = Math.floor(Math.random() * 10 + 1);
-    let randomNumber3 = Math.floor(Math.random() * 10 + 1);
+    const randomNumber2 = Math.floor(Math.random() * 10 - 1);
+    
+    let randomNumber3 = Math.floor(Math.random() * 10 );
 
     const rN3 = randomNumber3;
 
-    let progression = `${randomNumber3}`;
+    let progression = ``;
     let rightAnswer;
 
-    for (let z = 0; z <= 8; z += 1) {
+    for (let z = 0; z < 10; z += 1) {
       if (z === randomNumber2) {
         randomNumber3 += rN3;
         rightAnswer = randomNumber3;
